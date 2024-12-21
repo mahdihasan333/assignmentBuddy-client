7268535113ccf2716c7223cf81a59db6089df58a
popyakter121 <popy2000akter@gmail.com>
2024-12-22 02:03:55 +0500

    added faq questions section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import Faq from '../../components/Faq';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Faq/>
         </div>
     );
 };
