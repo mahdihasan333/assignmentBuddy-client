5ade31c50956345f176747383679f315f8e6eb76
popyakter121 <popy2000akter@gmail.com>
2024-12-22 10:19:10 +0500

    added private route

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,4 +1,12 @@
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { createContext, useState } from "react";
 import { app } from "../firebase/firebase.config";
 import { useEffect } from "react";
@@ -6,34 +14,34 @@ import { useEffect } from "react";
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  console.log(user)
+  console.log(user);
   const [loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
 
-//   create new user
+  //   create new user
   const createNewUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   // login user
   const loginUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   // login with google
   const loginWithGoogle = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
   // logout user
   const logoutUser = () => {
+    setLoading(true);
     return signOut(auth);
   };
-
-
 
   const authInfo = {
     user,
@@ -46,17 +54,16 @@ const AuthProvider = ({ children }) => {
     logoutUser,
   };
 
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, currentUser => {
-        console.log('CurrentUser -->', currentUser)
-        setUser(currentUser)
-        setLoading(false)
-    })
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("CurrentUser -->", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
     return () => {
-        unsubscribe();
-    }
-  }, [])
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const PrivateRoute = ({children}) => {
 
     const {user, loading} = useContext(AuthContext)
+
+    if(loading) {
+        return <Spinner/>
+    }
 
     if(user) {
         return children
